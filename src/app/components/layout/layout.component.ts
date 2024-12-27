1e82820fd87978a86a4c1c081468ad902e0a9b08
Nitin Sawale <nitinsawale111@gmail.com>
2024-12-27 18:55:14 +0530

    added sidebar

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, SidebarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
